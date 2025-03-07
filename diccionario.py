e0ccef2bb5fc69de1d09606918c1120da146f1b9
ChristianGarcia456 <kikigarciaramirez68@gmail.com>
2025-03-07 16:23:30 -0500

    Create diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "PARCHARSE": "ir con amigos a relajarce",
+            "PARCERO": "persona amiga o conocida",
+            "LLAVE": "mejor amigo"
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
